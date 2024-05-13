316c8bdf94df2a0daa3fc4993d9d6b9929d4c1b7
Hershal0-0 <hershalrao@gmail.com>
2024-05-14 01:08:46 +0530

    changes in favicon and product images

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("333a500caf046e7b")
+/******/ 		__webpack_require__.h = () => ("50dbb6c0bb98bf57")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a840be03a3bb3634"; }
+/******/ 		__webpack_require__.h = function() { return "7f707e2bf9b8d2ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
